0fec83dcfda9460ff7e9a88961dc873bc3102ac7
Seymour James <seymourpjames@gmail.com>
2022-05-16 01:57:37 -0700

    warning fix

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Aos from "aos";
 import "aos/dist/aos.css";
